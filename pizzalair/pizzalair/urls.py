49e0d137ee82fd996e35a8993e5e272acfd1521c
SpicyBeard <103431080+SpicyBeard@users.noreply.github.com>
2023-05-02 16:30:11 +0000

    migrate completed

--- pizzalair/pizzalair/urls.py
+++ pizzalair/pizzalair/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls'))
+    #path('user/', include('user.urls'))
 ]
