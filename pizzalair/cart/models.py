e8b1f18db3f98997956f5d71583dabaff732d643
Rebekkaloga <112619790+Rebekkaloga@users.noreply.github.com>
2023-05-03 16:07:36 +0000

    Cart

--- pizzalair/cart/models.py
+++ pizzalair/cart/models.py
@@ -1,10 +1,16 @@
+import datetime
+
 from django.db import models
 from products.models import Product
-#from users.models import User
+from user.models import User
 
 # Create your models here.
 class Cart(models.Model):
-    #user = models.ForeignKey(User, on_delete = models.SET_NULL)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    created_at = models.DateField(defult = datetime)
+
+class CartItem(models.Model):
     products = models.ManyToManyField(Product)
-    amount = models.IntegerField(default=0)
+    amount = models.IntegerField(default=1)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
 
--- pizzalair/cart/views.py
+++ pizzalair/cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from .models import Cart, CartItem
 
 # Create your views here.
+class DetailCart(DetailView):
+    model = Cart
+    template_name='detail_cart.html'
