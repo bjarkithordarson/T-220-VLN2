27ca0168a6dbeaa0d2ac22140cd0a0ac7506e1ac
Rebekkaloga <112619790+Rebekkaloga@users.noreply.github.com>
2023-05-06 15:29:48 +0000

    Delete migrations

--- pizzalair/pizzalair/settings.py
+++ pizzalair/pizzalair/settings.py
@@ -132,6 +132,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "users.User"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- pizzalair/users/models.py
+++ pizzalair/users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User as AU
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
-class User(models.Model):
-    user = models.OneToOneField(AU, default=0, on_delete=models.CASCADE)
+class User(AbstractUser):
+    #user = models.OneToOneField(on_delete=models.CASCADE, null=True)
     loyalty_points = models.IntegerField(default=0)
     profile_picture = models.ImageField(null=True, default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
 
