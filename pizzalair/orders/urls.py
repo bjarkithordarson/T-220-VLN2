21c8727fa1067e72a02981732b678fc62f2d7b29
Bjarki Rafn Þórðarson <bjarki@thordarson.is>
2023-05-07 11:23:23 +0000

    Scaffolding

--- pizzalair/orders/urls.py
+++ pizzalair/orders/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('registere', views.registere, name='registere')
+    path('', views.index, name='index'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('<int:order_id>/', views.order, name='order'),
+    path('<int:order_id>/payment/', views.payment, name='payment'),
+    path('<int:order_id>/billing/', views.billing, name='billing'),
+    path('<int:order_id>/review/', views.review, name='review'),
+    path('<int:order_id>/confirmation/', views.confirmation, name='confirmation')
 ]
--- pizzalair/orders/views.py
+++ pizzalair/orders/views.py
@@ -1,13 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
 # Create your views here.
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            from.save()
-            return redirect('login')
-    return render(request, 'template/registere.html', {
-        'form': UserCreationForm()
-    })
+@login_required
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def checkout(request):
+    return render(request, 'checkout.html')
+
+@login_required
+def order(request):
+    return render(request, 'order.html')
+
+@login_required
+def payment(request):
+    return render(request, 'payment.html')
+
+@login_required
+def billing(request):
+    return render(request, 'billing.html')
+
+@login_required
+def review(request):
+    return render(request, 'review.html')
+
+@login_required
+def confirmation(request):
+    return render(request, 'confirmation.html')
