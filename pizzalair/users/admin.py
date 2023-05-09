ec5d6c97db3ceb500613c567eda2f7be99d5c42b
Bjarki Rafn Þórðarson <bjarki@thordarson.is>
2023-05-09 10:54:22 +0000

    Admin classes

--- pizzalair/users/admin.py
+++ pizzalair/users/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import User
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
 
-admin.site.register(User)
+
+admin.site.register(User, UserAdmin)
