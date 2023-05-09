aff048ec790e71e00bcd1b77bc3eb42ba9137610
SpicyBeard <103431080+SpicyBeard@users.noreply.github.com>
2023-05-09 08:12:31 +0000

    products
    searching is done!

--- pizzalair/products/urls.py
+++ pizzalair/products/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<int:product_id>/", views.product_details, name="product_details"),
     path("category/<slug:slug>/", views.category, name="category"),
+    path("search/", views.search, name="search"),
     path("<int:product_id>/popup", views.product_details, name="product_details_popup")
 ]
