1786839cee88064966a500319a8427658bf33688
SpicyBeard <103431080+SpicyBeard@users.noreply.github.com>
2023-05-10 14:35:37 +0000

    items environment
    changes

--- pizzalair/products/urls.py
+++ pizzalair/products/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     path("pizzas/", views.pizza_list, name="pizza_list"),
     path("offers/", views.offer_list, name="offer_list"),
     path("merch/", views.merch_list, name="merch_list"),
+    path("<slug:slug>/", views.product_list, name="product_list"),
     path("pizzas/<int:pizza_id>/", views.pizza_details, name="pizza_details"),
     path("offers/<int:offer_id>/", views.offer_details, name="offer_details"),
     path("offers/create", views.offer_create, name="offer_create"),
     path("merch/<int:merch_id>/", views.merch_details, name="merch_details"),
-    path("categories/<slug:slug>/", views.category, name="category"),
+    # path("categories/<slug:slug>/", views.category, name="category"),
     path("<int:product_id>/popup", views.product_details, name="product_details_popup")
 
 ]
